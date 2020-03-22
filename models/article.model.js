e6fb336380a7072df1d257f32bee1d0d401ffd33
Manohar <hi4manohar@gmail.com>
2020-03-22 12:42:23 +0530

    updated class modules

--- models/article.model.js
+++ models/article.model.js
@@ -6,7 +6,20 @@ class article {
 
 	getArticleList() {
 		return new Promise((resolve, reject) => {
-			let sql = "SELECT post_title, post_content, post_date FROM `wp_posts` where post_status='publish' and post_type='post'  LIMIT 0, 20";
+			let sql = `SELECT wp_posts.*, p.guid FROM wp_posts 
+			LEFT JOIN wp_postmeta as pm ON 
+				wp_posts.ID=pm.post_id AND pm.meta_key='_thumbnail_id'
+			LEFT JOIN wp_posts as p ON
+				pm.meta_value=p.ID
+
+			WHERE wp_posts.post_type="post" AND wp_posts.post_status = "publish" AND wp_posts.ID IN (
+			    SELECT object_id FROM wp_term_relationships WHERE term_taxonomy_id IN (
+			        SELECT term_taxonomy_id FROM wp_term_taxonomy WHERE taxonomy = "category" AND term_id IN (
+			            SELECT t.term_id FROM wp_terms t WHERE t.slug IN ("java-tutorial")
+			        )
+			    )
+			)
+			LIMIT 0, 20`;
 			this.dbinst.query(sql, function (err, result) {
 				if(err) {
 					reject({
