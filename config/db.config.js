6991cf39983d9538770899abb2420b8ad8aeb019
Manohar <hi4manohar@gmail.com>
2020-04-07 02:41:56 +0530

    updated db

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     connection: {
-        host: '34.227.18.153',
+        host: '54.224.119.94',
         user: 'root',
         password: 'manoharkjde2434738',
         database: 'edunotes-admin',
