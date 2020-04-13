4ab490a238d636af4ee51b0aac50688e56d26d51
Manohar <hi4manohar@gmail.com>
2020-04-13 22:09:50 +0530

    updated filter

--- models/answers.model.js
+++ models/answers.model.js
@@ -6,36 +6,12 @@ class answers {
 
     getAnswersList(param) {
         return new Promise((resolve, reject) => {
-            // let sql = `SELECT 
-			// 	wp_posts.ID,
-			// 	wp_posts.post_author,
-			// 	wp_posts.post_date,
-			// 	wp_posts.post_content,
-			// 	wp_posts.post_title,
-			// 	wp_posts.post_status,
-			// 	wp_posts.post_name,
-			// 	wp_posts.menu_order,
-			// 	wp_posts.post_type,
-			// 	wp_posts.post_mime_type,
-			// 	CONCAT(
-			// 		'https://s3.amazonaws.com/edunotes-media/',
-			// 		s3t.path
-			// 	) as guid FROM wp_posts 
-			// LEFT JOIN wp_postmeta as pm ON 
-			// 	wp_posts.ID=pm.post_id AND pm.meta_key='_thumbnail_id'
-			// LEFT JOIN wp_as3cf_items as s3t ON 
-			// 		pm.meta_value = s3t.source_id
 
-			// WHERE wp_posts.post_type="post" AND wp_posts.post_status = "publish" AND wp_posts.ID IN (
-			//     SELECT object_id FROM wp_term_relationships WHERE term_taxonomy_id IN (
-			//         SELECT term_taxonomy_id FROM wp_term_taxonomy WHERE taxonomy = "category" AND term_id IN (
-            //             SELECT t.term_id FROM wp_terms t WHERE 
-            //             t.slug IN ("${param.class}", "answers", "${param.board}")
-			//         )
-			//     ) GROUP by (object_id)
-			// 		having COUNT(object_id) = 3
-			// )
-            // LIMIT ${param.page}, ${param.offset}`;
+            let slugToMatch = [param.class, "answers", param.board];
+
+            if( param.tags ) {
+                slugToMatch.push([param.tags]);
+            }
 
             let sql = `SELECT 
                     wp_posts.ID,
@@ -66,21 +42,65 @@ class answers {
             WHERE wp_posts.post_type="post" AND wp_posts.post_status = "publish" AND wp_posts.ID IN (
                 SELECT object_id FROM wp_term_relationships WHERE term_taxonomy_id IN (
                     SELECT term_taxonomy_id FROM wp_term_taxonomy WHERE 
-                    taxonomy = "category" AND term_id IN (
+                    taxonomy IN("category", "post_tag") AND term_id IN (
                         SELECT t.term_id FROM wp_terms t WHERE
-                        t.slug IN ("${param.class}", "answers", "${param.board}")
+                        t.slug IN ("${slugToMatch.join('","')}")
                     )
-                ) GROUP by (object_id) having COUNT(object_id) = 3
+                ) GROUP by (object_id) having COUNT(object_id) = ${slugToMatch.length}
             ) GROUP by(ID) LIMIT ${param.page}, ${param.offset}`;
+            console.log(sql);
             this.dbinst.query(sql, function (err, result) {
                 if (err) {
                     reject({
-                        status: true,
+                        status: false,
                         msg: err
                     })
                 } else {
                     resolve({
+                        status: true,
+                        data: result
+                    })
+                }
+            });
+        })
+    }
+
+    getTagsofAnswers(param) {
+        return new Promise((resolve, reject) => {
+
+            let slugToMatch = [param.class, "answers", param.board];
+
+            let sql = `SELECT
+                t.name as tags, t.slug as slugs, t.term_id as ids
+            FROM wp_posts
+                INNER JOIN wp_term_relationships as wtr ON wtr.object_id = wp_posts.ID
+                LEFT JOIN wp_term_taxonomy as tt ON (
+                    tt.term_id = wtr.term_taxonomy_id and tt.taxonomy = "post_tag"
+                )
+                LEFT JOIN wp_terms as t ON t.term_id = tt.term_id WHERE
+                wp_posts.post_type = "post"
+                AND wp_posts.post_status = "publish"
+                AND wp_posts.ID IN (
+                    SELECT object_id FROM wp_term_relationships
+                    WHERE term_taxonomy_id IN (
+                    SELECT term_taxonomy_id FROM wp_term_taxonomy
+                    WHERE taxonomy = "category" AND term_id IN (
+                            SELECT t.term_id FROM wp_terms t WHERE
+                                t.slug IN ("${slugToMatch.join('","')}")
+                        )
+                    ) GROUP by (object_id) having COUNT(object_id) = ${
+                      slugToMatch.length
+                    }
+                ) AND t.name IS NOT NULL GROUP by (tags)`;
+            this.dbinst.query(sql, function (err, result) {
+                if (err) {
+                    reject({
                         status: false,
+                        msg: err
+                    })
+                } else {
+                    resolve({
+                        status: true,
                         data: result
                     })
                 }
--- routes/answers.route.js
+++ routes/answers.route.js
@@ -12,13 +12,18 @@ router.get('/list', async function (req, res, next) {
     param.board = req.appBaseConfig.board;
     param.page = req.query.page ? (req.query.page) * 20 : 0;
     param.offset = 20;
+    param.tags = req.query.tags ? req.query.tags.trim() : false;
 
     try {
         let answersData = await answersObj.getAnswersList(param);
-
+        let getAnswerTags = false;
+        if(param.page === 0) {
+            getAnswerTags = await answersObj.getTagsofAnswers(param);
+        }
         res.json({
             status: true,
-            data: answersData.data
+            data: answersData.data,
+            tags: (getAnswerTags && getAnswerTags.data.length > 0 ) ? getAnswerTags.data : []
         });
     } catch (err) {
         console.log(err);
